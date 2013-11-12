b5532cc55fee5cccbc8dddfa38c48e96f45c6ecd
Leo Liu <sdl.web@gmail.com>
2013-11-12 23:01:33 +0800

    Fix #20: Incorrect generation of relative file names

--- ggtags.el
+++ ggtags.el
@@ -517,11 +517,15 @@ With a prefix arg (non-nil DEFINITION) always find definitions."
           (and (ggtags-find-project)
                (not (ggtags-project-has-rtags (ggtags-find-project)))))
       (ggtags-find-tag 'definition name)
-    (ggtags-find-tag (format "--from-here=%d:%s"
-                             (line-number-at-pos)
-                             (shell-quote-argument
-                              (file-relative-name buffer-file-name)))
-                     name)))
+    (ggtags-find-tag
+     (format "--from-here=%d:%s"
+             (line-number-at-pos)
+             (shell-quote-argument
+              ;; Note `ggtags-global-start' binds default-directory to
+              ;; project root.
+              (file-relative-name buffer-file-name
+                                  (ggtags-current-project-root))))
+     name)))
 
 (defun ggtags-find-reference (name)
   (interactive (list (ggtags-read-tag)))
